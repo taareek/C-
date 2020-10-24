b7b80230d73cbecde6572d9704180366c6a7ca08
Tarek Aziz <60188460+taareek@users.noreply.github.com>
2020-10-24 23:50:36 +0600

    Add files via upload

---
+++ a largest smallest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int A[10], smallest, largest;
+for(int i=0; i<10; i++){
+    cin>>A[i];
+}
+largest=A[0];
+smallest=A[0];
+for(int i=0; i<10; i++){
+    if(A[i]> largest){
+        largest= A[i];
+    }
+    if(A[i]<smallest){
+        smallest=A[i];
+    }
+}
+cout<<"Largest: "<<largest<<endl;
+cout<<"Smallest: "<<smallest;
+}
